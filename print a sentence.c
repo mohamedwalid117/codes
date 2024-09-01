5cc2c3c2a8c95b4e052c3651180c8c0f11a52649
mohamed walid <140534792+mohamedwalid117@users.noreply.github.com>
2024-09-01 23:04:00 +0300

    Add files via upload
    task1

---
+++ print a sentence.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : print.c
+ Author      : mowalid
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	 printf("C Programming");
+	return 0;
+}
