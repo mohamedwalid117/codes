fe569e6c66c09e3695fe7272c96daccbbb95a1ac
mowalid <mo@mail>
2024-09-05 01:20:09 +0300

    new task added

---
+++ UNIT 1/LESSON3 C_BASICS/print an integer/src/print an integer.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : print.c
+ Author      : mowalid
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int integer =(1,2,3);
+
+	printf(" %d ",integer);
+	return 0;
+}
